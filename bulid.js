f6281d116eef7d18435a6f7552c59224e3b9fad8
amol234545 <amolmilton@gmail.com>
2022-12-29 13:53:31 -0500

    Update bulid.js

--- bulid.js
+++ bulid.js
@@ -1,1 +1,9 @@
+const exe = require('@angablue/exe');
 
+const build = exe({
+    entry: './index.js',
+    out: './build/My Cool App.exe',
+    target: 'latest-win-x64'
+});
+
+build.then(() => console.log('Build completed!'));
