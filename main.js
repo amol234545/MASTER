b46ed8667779d63693c1479d741c7bf0ec7c8a86
amol234545 <amolmilton@gmail.com>
2022-12-29 03:52:34 +0000

    k

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const got_strap = async () => (await import("got")).got
 got_strap().then(got => {
-  got.get("https://google.com").text().then(html => console.log)
+  got.get("https://google.com").text().then(html => console.log(html))
 })
