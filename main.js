c64da3d1b25fdac009a1e3a3eb9469850d4bceb1
amol234545 <amolmilton@gmail.com>
2022-12-30 01:15:19 +0000

    hello

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Helo")
+const prompt = require("prompt-sync")({sigint:true})
+const name = prompt("What is your name?")
+const log = console.log
+const warn = console.log
+const error = console.error
+const wait = (seconds) => {new Promise((resolve, reject) => {
+    setTimeout(resolve,seconds * 1000)
+})}
