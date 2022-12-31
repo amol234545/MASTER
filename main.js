d999955541312c8ea3a8655dcad0218ee8ea4417
amol234545 <amolmilton@gmail.com>
2022-12-31 18:23:33 -0500

    hello

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const prompt = require("prompt-sync")({sigint:true})
-const name = prompt("What is your name?")
+const process = require("node:process")
 const log = console.log
 const warn = console.log
 const error = console.error
+function readArgs() {
+    return process.argv
+}
