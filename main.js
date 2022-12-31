4a178112de70d8c5e69ff89d4a259207164029dc
amol234545 <amolmilton@gmail.com>
2022-12-31 18:14:06 -0500

    hello

--- main.js
+++ main.js
@@ -3,6 +3,3 @@ const name = prompt("What is your name?")
 const log = console.log
 const warn = console.log
 const error = console.error
-const wait = seconds => setTimeout(() => {},seconds * 1000)
-log("Helo " + name)
-
