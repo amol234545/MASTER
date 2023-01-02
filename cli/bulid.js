0d631d275bdb49d127f0f716bf40d1877432a190
amol234545 <amolmilton@gmail.com>
2023-01-02 13:42:58 -0500

    Changed

--- cli/bulid.js
+++ cli/bulid.js
@@ -3,7 +3,8 @@ const exe = require('@angablue/exe');
 const build = exe({
     entry: './main.js',
     out: '/home/runner/work/MASTER/build/cli/connect.exe',
-    target: 'latest-win-x64'
+    target: 'latest-win-x64',
+    icon: './icon.ico'
 });
 
 build.then(() => console.log('Build completed!'));
